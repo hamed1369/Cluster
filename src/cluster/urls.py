6d53ffafb06260083daf81e6b43180556624c62d
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-03 17:24:40 +0430

    initial static and template

--- src/cluster/urls.py
+++ src/cluster/urls.py
@@ -2,16 +2,23 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from cluster import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'cluster.views.home', name='home'),
-    url(r'^cluster/', include('cluster.registration.urls')),
+                       # url(r'^$', 'cluster.views.home', name='home'),
+                       url(r'^cluster/', include('cluster.registration.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('cluster.accounts.urls')),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^accounts/', include('cluster.account.urls')),
 )
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                             {'document_root': settings.STATIC_ROOT}),
+    )
