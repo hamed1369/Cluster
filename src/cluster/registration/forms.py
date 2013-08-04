cf24aed5e042309e5170f7981e13fe7d43f6c70e
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-04 04:45:32 +0430

    init register formsets

--- src/cluster/registration/forms.py
+++ src/cluster/registration/forms.py
@@ -1,17 +1,56 @@
 # -*- coding:utf-8 -*-
 from django import forms
-from cluster.account.account.models import Account
+from cluster.account.account.models import Member
+from cluster.account.personal_info.models import EducationalResume, Publication, Invention, \
+    ExecutiveResearchProject, LanguageSkill, SoftwareSkill
 
 __author__ = 'M.Y'
 
 
 class RegisterForm(forms.ModelForm):
     is_cluster = forms.BooleanField(required=False,
-                                    label=u"آیا درخواست ثبت خوشه وجود دارد؟(در صورت تایید و ارسال فرم ثبت نام برای اعضاء خوشه)")
+                                    label=
+                                    u"آیا درخواست ثبت خوشه وجود دارد؟(در صورت تایید و ارسال فرم ثبت نام برای اعضاء خوشه)", )
 
     class Meta:
-        model = Account
+        model = Member
 
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
+
+
+class MemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
+
+
+class EducationalResumeForm(forms.ModelForm):
+    class Meta:
+        model = EducationalResume
+
+
+class PublicationForm(forms.ModelForm):
+    class Meta:
+        model = Publication
+
+
+class InventionForm(forms.ModelForm):
+    class Meta:
+        model = Invention
+
+
+class ExecutiveResearchProjectForm(forms.ModelForm):
+    class Meta:
+        model = ExecutiveResearchProject
+
+
+class LanguageSkillForm(forms.ModelForm):
+    class Meta:
+        model = LanguageSkill
+
+
+class SoftwareSkillForm(forms.ModelForm):
+    class Meta:
+        model = SoftwareSkill
+
 
--- src/cluster/registration/views.py
+++ src/cluster/registration/views.py
@@ -1,10 +1,31 @@
-# Create your views here.
+# -*- coding:utf-8 -*-
+from django.forms.formsets import formset_factory
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from cluster.registration.forms import RegisterForm
+from cluster.registration.forms import RegisterForm, MemberForm, EducationalResumeForm, PublicationForm, \
+    InventionForm, ExecutiveResearchProjectForm, LanguageSkillForm, SoftwareSkillForm
 
 
 def register(request, cluster_id=None):
     register_form = RegisterForm()
-    return render_to_response('registration/register.html', {'register_form': register_form},
+    cluster_member_formset = formset_factory(MemberForm)
+    resume_formset = formset_factory(EducationalResumeForm)
+    publication_formset = formset_factory(PublicationForm)
+    invention_formset = formset_factory(InventionForm)
+    executive_research_formset = formset_factory(ExecutiveResearchProjectForm)
+    language_skill_formset = formset_factory(LanguageSkillForm)
+    software_skill_formset = formset_factory(SoftwareSkillForm)
+
+    c = {
+        'register_form': register_form,
+        'cluster_member_formset': cluster_member_formset,
+        'resume_formset': resume_formset,
+        'publication_formset': publication_formset,
+        'invention_formset': invention_formset,
+        'executive_research_formset': executive_research_formset,
+        'language_skill_formset': language_skill_formset,
+        'software_skill_formset': software_skill_formset,
+    }
+    return render_to_response('registration/register.html',
+                              c,
                               context_instance=RequestContext(request))
