35cdc3c9c79d7b8ec3baedbe2fef79747f406afe
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-04 05:34:15 +0430

    put initial theme on website

--- static/base.js
+++ static/base.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('input[type=text]').addClass('form-control');
     $('input[type=password]').addClass('form-control');
-    $('.formset_container').formset();
+    if (typeof formset != 'undefined')
+        $('.formset_container').formset();
 });
