de0ec64d9d44724f594ee693cd497757c49fa019
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-09 02:23:23 +0430

    cluster form and personal form created

--- src/cluster/registration/forms.py
+++ src/cluster/registration/forms.py
@@ -10,24 +10,38 @@ __author__ = 'M.Y'
 class ClusterForm(forms.Form):
     BOOLEAN_CHOICES = (
         (1, u"بله"),
-        (1, u"خیر"),
+        (2, u"خیر"),
     )
     is_cluster = forms.ChoiceField(required=False, choices=BOOLEAN_CHOICES, widget=forms.RadioSelect(),
                                    label=
                                    u"آیا درخواست ثبت خوشه وجود دارد؟(در صورت تایید و ارسال فرم ثبت نام برای اعضاء خوشه)",
     )
 
+    name = forms.CharField(required=False, label=u"نام خوشه (خوشه + حوزه فعالیت + دانشگاه یا موسسه)")
+
     def __init__(self, *args, **kwargs):
         super(ClusterForm, self).__init__(*args, **kwargs)
 
 
 class RegisterForm(forms.ModelForm):
+    BOOLEAN_CHOICES = (
+        (True, u"بله"),
+        (False, u"خیر"),
+    )
+
     class Meta:
         model = Member
         exclude = ('cluster', 'user')
 
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields.insert(0, 'first_name', forms.CharField(required=True, label=u"نام"))
+        self.fields.insert(1, 'last_name', forms.CharField(required=True, label=u"نام خانوادگی"))
+        self.fields.insert(2, 'username', forms.CharField(required=True, label=u"نام کاربری"))
+        self.fields.insert(3, 'email', forms.EmailField(label=u"پست الکترونیک"))
+        self.fields['foundation_of_elites'] = forms.ChoiceField(required=True, choices=RegisterForm.BOOLEAN_CHOICES,
+                                                                widget=forms.RadioSelect(), )
+        self.fields['foundation_of_elites'].label = u"آیا عضو بنیاد ملی نخبگان می باشید؟"
         for field in self.fields:
             if self.fields[field].required:
                 self.fields[field].widget.attrs.update({'class': 'validate[required,] text-input'})
--- static/registration/register.js
+++ static/registration/register.js
@@ -10,4 +10,49 @@ $(document).ready(function () {
     );
 
     $("#register_form").validationEngine({promptPosition: "centerLeft", scroll: false});
+
+
+    $('input[name=is_cluster]').change(function () {
+        var is_cluster = $('input[name=is_cluster]:checked', '#register_form').val();
+
+        if (is_cluster == 1) {
+            $('#only_for_cluster').slideDown();
+        } else {
+            $('#only_for_cluster').slideUp();
+        }
+
+    });
+
+    $('select[name*="employment_status"]').change(function () {
+        if ($(this).val() == 1) {
+            $('input[name*="organization"]').parents('tr').first().fadeIn();
+        } else {
+            $('input[name*="organization"]').parents('tr').first().fadeOut();
+        }
+    });
+
+    $('select[name*="military_status"]').change(function () {
+        if ($(this).val() == 2) {
+            $('select[name*="exemption_type"]').parents('tr').first().fadeOut();
+            $('input[name*="military_place"]').parents('tr').first().fadeIn();
+        } else if ($(this).val() == 3) {
+            $('select[name*="exemption_type"]').parents('tr').first().fadeIn();
+            $('input[name*="military_place"]').parents('tr').first().fadeOut();
+        } else {
+            $('select[name*="exemption_type"]').parents('tr').first().fadeOut();
+            $('input[name*="military_place"]').parents('tr').first().fadeOut();
+        }
+    });
+
+
+    $('#register_table tr').click(function () {
+        $('#register_table tr').css({
+            border: '0',
+            backgroundColor: '#fff'
+        });
+        $(this).css({
+            border: '1px solid rgb(221, 221, 0)',
+            backgroundColor: 'rgb(255, 253, 226)'
+        });
+    });
 });
