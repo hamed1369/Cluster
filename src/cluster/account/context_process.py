6190db597c8d8c5c6ffe2d9f36dd96688567ce09
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-11 20:19:36 +0430

    login form in base added

---
+++ src/cluster/account/context_process.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from cluster.account.forms import SignInForm
+
+__author__ = 'M.Y'
+
+
+def login_form_check(request):
+    if not hasattr(request, 'login_form'):
+        path = request.path
+        if request.method == 'POST' and request.POST.get('login-submit'):
+            login_form = SignInForm(request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data['username']
+                password = login_form.cleaned_data['password']
+
+                user = authenticate(username=username, password=password)
+                if user is None or not user.is_active:
+                    messages.error(request, u"نام کاربری یا گذرواژه نادرست است.")
+                else:
+                    login(request, user)
+                    return HttpResponseRedirect(path)
+
+        else:
+            login_form = SignInForm()
+
+        return {'login_form': login_form}
+    return {}
--- src/cluster/account/templatetags/account_template_tags.py
+++ src/cluster/account/templatetags/account_template_tags.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.filter
 def get_dict(input_dict, key):
     return input_dict.get(key)
+
+
+@register.simple_tag
+def welcome_st(user):
+    return u"%s %s عزیز،<br/> خوش آمدید" % (user.first_name, user.last_name)
