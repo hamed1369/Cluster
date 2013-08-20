bcf02487d3f182454978e2ad227d2bf00f756807
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-20 10:30:12 +0430

    inline commit

--- src/cluster/utils/manager/views.py
+++ src/cluster/utils/manager/views.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from cluster.utils.manager.main import manager_children
 
 __author__ = 'M.Y'
 
 
 @login_required
 def process_main_page(request, manager_name):
-    return render_to_response('manager/main.html', {}, context_instance=RequestContext(request))
+    for manager in manager_children:
+        if manager.manager_name == manager_name:
+            manager
+
+        return render_to_response('manager/main.html', {}, context_instance=RequestContext(request))
+    raise Http404()
 
 
 @login_required
