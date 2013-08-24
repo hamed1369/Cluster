c585430cec2dee6ad353a80da400ab86152bb37d
Tahmooresi <tahmooresi@arsh.co>
2013-08-25 00:27:07 +0430

    b

--- src/cluster/settings.py
+++ src/cluster/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Iran/Tehran'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -101,6 +101,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     'cluster.account.context_process.login_form_check',
     'cluster.account.context_process.default_context',
+    'django.core.context_processors.tz',
 )
 ROOT_URLCONF = 'cluster.urls'
 LOGIN_URL = '/accounts/login/'
@@ -116,6 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'suit',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -125,7 +127,6 @@ INSTALLED_APPS = (
     'cluster.account',
     'cluster.project'
 )
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
