9ee15e5547525d2ffcb9fdf5e8771f2bd5bf3251
mohamadsaleh yasheikhakbari <s_akbari@arsh.co.ir>
2013-08-24 09:25:02 +0430

    menu system changed

--- src/cluster/account/templatetags/account_template_tags.py
+++ src/cluster/account/templatetags/account_template_tags.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
+from django.core.urlresolvers import reverse
+
 __author__ = 'M.Y'
 from django import template
+from cluster import settings
 
 register = template.Library()
 
@@ -12,10 +15,25 @@ def get_dict(input_dict, key):
 
 @register.simple_tag
 def welcome_st(user):
-    overall_name = u"%s %s"%(user.first_name, user.last_name) if (user.first_name and user.last_name) else u"%s"%user.username
+    overall_name = u"%s %s" % (user.first_name, user.last_name) if (
+        user.first_name and user.last_name) else u"%s" % user.username
     return u"%s خوش آمدید." % overall_name
+
 
 @register.filter
 def is_false(value):
     return value is False or value == 'False'
 
+
+@register.simple_tag(takes_context=True)
+def render_url_li(context, name, persian_name):
+    url = reverse(name)
+    html_class = 'active' if context.get('request').path == url else ''
+    res = u"""
+    <li>
+        <a href="%s%s" class="%s">
+            %s
+        </a>
+    </li>
+    """ % (settings.SITE_URL, url, html_class, persian_name)
+    return res
