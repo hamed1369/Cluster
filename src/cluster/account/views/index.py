d6b504cfef7ff693082d1b424782b31f2650b825
Tahmooresi <tahmooresi@arsh.co>
2013-09-04 15:04:23 +0430

    intro page changed alot

--- src/cluster/account/views/index.py
+++ src/cluster/account/views/index.py
@@ -4,10 +4,12 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from cluster.account.forms import SignInForm
 
 __author__ = 'M.Y'
 
 
 
 def index(request):
-    return render_to_response('intro.html', {}, context_instance=RequestContext(request))
+    login_form = SignInForm()
+    return render_to_response('intro.html', {'login_form':login_form}, context_instance=RequestContext(request))
