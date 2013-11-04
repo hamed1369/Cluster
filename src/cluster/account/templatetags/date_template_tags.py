7a6939141826c527794c976ea9592b3fc261b931
Tahmooresi <tahmooresi@arsh.co>
2013-11-04 10:30:21 +0330

    some changes

--- src/cluster/account/templatetags/date_template_tags.py
+++ src/cluster/account/templatetags/date_template_tags.py
@@ -25,3 +25,14 @@ def pdate(date):
     sh_date = date_converter.jd_to_jalali(jd)
     st = str(sh_date[0])+"/"+str(sh_date[1])+"/"+str(sh_date[2])
     return u"%s"%st
+
+@register.simple_tag
+def get_current_date_time():
+    date = datetime.datetime.now()
+    date_converter = Calverter()
+    jd = date_converter.gregorian_to_jd(date.year, date.month, date.day)
+    sh_date = date_converter.jd_to_jalali(jd)
+    week_day = date_converter.jwday(jd)
+    day_name = JALALI_WEEKDAYS[week_day]
+    st = str(sh_date[0])+"/"+str(sh_date[1])+"/"+str(sh_date[2])
+    return u"اکنون، %s %s ساعت %s و %s دقیقه"%(day_name,st,date.time().hour,date.time().minute)
