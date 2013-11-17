73a38913366161f09aebb7d86e4de298c3b38d41
Tahmooresi <tahmooresi@arsh.co>
2013-11-17 18:35:10 +0330

    bug

--- src/cluster/account/views/index.py
+++ src/cluster/account/views/index.py
@@ -13,6 +13,7 @@ def index(request):
     login_form = SignInForm()
     news_list = News.objects.all().order_by('-publish_date')
     links_list = Link.objects.all().order_by('order')
+    has_submited = False
     if request.method == 'POST':
         contact_form = ContactForm(request.POST, prefix='contact')
         if contact_form.is_valid():
@@ -20,7 +21,6 @@ def index(request):
             has_submited = True
             contact_form = ContactForm(prefix='contact')
     else:
-        has_submited = False
         contact_form = ContactForm(prefix='contact')
     return render_to_response('intro.html',
                               {'login_form': login_form,'has_submited':has_submited, 'news_list': news_list,'links_list': links_list, 'contact_form': contact_form},
