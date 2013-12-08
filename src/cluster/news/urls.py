56cff4458011118d01af9cf79dead72412fbb527
tahmooresi <tahmooresi@arsh.co>
2013-12-08 13:11:42 +0330

    bug

--- src/cluster/news/urls.py
+++ src/cluster/news/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('cluster.news.views',
-                       url(r'^/(?P<news_id>\d)/$', 'news_detail', name='news_detail'),
+                       url(r'^/(?P<news_id>\d+)/$', 'news_detail', name='news_detail'),
 
 )
